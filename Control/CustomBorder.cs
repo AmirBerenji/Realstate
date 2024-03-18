8e1d9941613a03e71eeeadf9483d33a1a69647e7
Berenji <a.berenji62@gmail.com>
2024-03-18 09:38:25 -0700

    Create Animation in Detail

--- Control/CustomBorder.cs
+++ Control/CustomBorder.cs
@@ -11,9 +11,17 @@ namespace Realstate.Control
     {
         public CustomBorder()
         {
+            TranslationX= new Random().Next(-500,500);
+            Rotation = Math.Max(TranslationX, 100);
             AddCornerRadius();
+
+            Loaded += (s, e) =>
+            {
+                this.TranslateTo(0, 0, 1500, Easing.SinInOut);
+                this.RotateTo(0, 1500, Easing.SinInOut);
+            };
         }
-        private List<int> CornerValues = new List<int> { 10, 40, 70, 100};
+        private List<int> CornerValues = new List<int> { 30, 30, 30, 30};
 
         private void AddCornerRadius()
         {
--- Model/PropertyRepository.cs
+++ Model/PropertyRepository.cs
@@ -65,7 +65,123 @@ namespace Realstate.Model
                 "https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg"}
 
             },
+           
+            
+            
+            
+            
+            new Property
+            {
+                Address = "Queens, New York",
+                Price = 550100,
+                DefaultImage ="https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg",
+                Images = new List<string> {
+                    "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg"}
 
+            },
+            new Property
+            {
+                Address = "Queens, New York",
+                Price = 550100,
+                DefaultImage ="https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg",
+                Images = new List<string> {
+                    "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg"
+                }
+
+            },
+
+            new Property
+            {
+                Address = "Queens, New York",
+                Price = 550100,
+                DefaultImage = "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                Images = new List<string> {
+                    "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg"}
+
+            },
+
+            new Property
+            {
+                Address = "Queens, New York",
+                Price = 550100,
+                DefaultImage = "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+                Images = new List<string> {
+                    "https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg",
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg"}
+
+            },
+
+
+             new Property
+            {
+                Address = "Queens, New York",
+                Price = 550100,
+                DefaultImage ="https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg",
+                Images = new List<string> {
+                    "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg"}
+
+            },
+            new Property
+            {
+                Address = "Queens, New York",
+                Price = 550100,
+                DefaultImage ="https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg",
+                Images = new List<string> {
+                    "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg"
+                }
+
+            },
+
+            new Property
+            {
+                Address = "Queens, New York",
+                Price = 550100,
+                DefaultImage = "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                Images = new List<string> {
+                    "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg"}
+
+            },
+
+            new Property
+            {
+                Address = "Queens, New York",
+                Price = 550100,
+                DefaultImage = "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+                Images = new List<string> {
+                    "https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg",
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg"}
+
+            },
         };
     }
 }
