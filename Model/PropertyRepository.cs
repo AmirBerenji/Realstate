cb074a71d6eaaf2d81b5c603cccad0de976e3de8
Berenji <a.berenji62@gmail.com>
2024-03-18 08:06:30 -0700

    Design Details Page

--- Model/PropertyRepository.cs
+++ Model/PropertyRepository.cs
@@ -15,7 +15,12 @@ namespace Realstate.Model
                 Address = "Queens, New York",
                 Price = 550100,
                 DefaultImage ="https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg",
-                Images = new List<string> { "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg", "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg" }
+                Images = new List<string> { 
+                    "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg", 
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg"}
 
             },
             new Property
@@ -23,7 +28,12 @@ namespace Realstate.Model
                 Address = "Queens, New York",
                 Price = 550100,
                 DefaultImage ="https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg",
-                Images = new List<string> { "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg", "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg" }
+                Images = new List<string> {
+                    "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg"}
 
             },
 
@@ -32,7 +42,12 @@ namespace Realstate.Model
                 Address = "Queens, New York",
                 Price = 550100,
                 DefaultImage ="https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg",
-                Images = new List<string> { "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg", "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg" }
+                Images = new List<string> {
+                    "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg"}
 
             },
 
@@ -41,7 +56,12 @@ namespace Realstate.Model
                 Address = "Queens, New York",
                 Price = 550100,
                 DefaultImage ="https://cdn.pixabay.com/photo/2023/08/11/04/51/fireworks-8182800_640.jpg",
-                Images = new List<string> { "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg", "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg" }
+                Images = new List<string> {
+                    "https://cdn.pixabay.com/photo/2014/11/21/17/17/house-540796_1280.jpg",
+                    "https://cdn.pixabay.com/photo/2016/11/18/17/20/living-room-1835923_640.jpg",
+                    "https://cdn.pixabay.com/photo/2018/04/23/14/35/window-3344407_1280.jpg",
+                "https://cdn.pixabay.com/photo/2018/01/11/22/35/window-3077128_960_720.jpg",
+                "https://cdn.pixabay.com/photo/2017/12/27/14/41/window-3042834_1280.jpg"}
 
             },
 
--- View/DetailsPage1.xaml.cs
+++ View/DetailsPage1.xaml.cs
@@ -1,3 +1,5 @@
+using Realstate.ViewModel;
+
 namespace Realstate.View;
 
 public partial class DetailsPage1 : ContentPage
@@ -5,5 +7,10 @@ public partial class DetailsPage1 : ContentPage
 	public DetailsPage1(Model.Property selectedProperty)
 	{
 		InitializeComponent();
+		var viewModel = new DetailesViewModel() { SelectedProperty = selectedProperty };
+		viewModel.PropertyImages = selectedProperty.Images.Take(2).ToList();
+		viewModel.MoreItems = selectedProperty.Images.Count - 2;
+
+		BindingContext = viewModel;
 	}
 }
